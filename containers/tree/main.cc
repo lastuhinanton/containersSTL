6fd10bebd3f1a3b8438b00c955f4cca534b54fbd
Daniella Mundungus <daniellm@va-c5.kzn.21-school.ru>
2023-09-11 10:36:09 +0300

    avlTree improved -> next step i would  chang to template

--- containers/tree/main.cc
+++ containers/tree/main.cc
@@ -2,30 +2,46 @@
 #include <map>
 #include <iostream>
 #include <cstdlib>
-#include "avlTree.h"
+#include "tree/avlTree.h"
 
 
 using namespace std;
 
+void remove_and_print(AVLTree *tree, int value){
+    tree->remove(value);
+    cout <<endl;
+    cout <<"### del "<< value <<  " ###"<< endl;
+    cout << "max = "<< tree->get_max() << endl;
+    cout << "min = "<< tree->get_min() << endl;
+    tree->print();
+    cout << "#################################";
+
+
+}
+
 int main(){
     AVLTree tree;
-    // tree.insert(0);
-    // tree.insert(2);
-    // tree.insert(1);
-    // tree.insert(10);
-    // tree.insert(8);
-    // tree.insert(14);
-    // tree.insert(11);
-    // tree.insert(19);
-    // tree.insert(13);
-    // tree.insert(-13);
+    tree.insert(0);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(10);
+    tree.insert(8);
+    tree.insert(14);
+    tree.insert(14);
+    tree.insert(11);
+    tree.insert(19);
+    tree.insert(13);
+    tree.insert(-13);
+    
 
-    for (int i =0; i < 100; i++){
-        int random = 1 + rand() % 99999;
-        tree.insert(random);
-    }
+    cout << "max = "<< tree.get_max() << endl;
+    cout << "min = "<< tree.get_min() << endl;
     tree.print();
 
+    remove_and_print(&tree, 14);
+    remove_and_print(&tree, 11);
+    remove_and_print(&tree, 0);
 
+    
     return 0;
 }
