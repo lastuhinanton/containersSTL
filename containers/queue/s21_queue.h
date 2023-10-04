2b8d7e581345ac4a11d65dee90bc05da12bbad34
Finch Marvolo <finchmar@va-c3.kzn.21-school.ru>
2023-10-04 12:58:03 +0300

    queue

--- containers/queue/s21_queue.h
+++ containers/queue/s21_queue.h
@@ -1,6 +1,61 @@
 #ifndef S21_QUEUE_H
 #define S21_QUEUE_H
 
-//init
+#include "../list/s21_list.h" 
+
+namespace s21 {
+
+template <typename T>
+class queue {
+ public:
+  using value_type = T;
+  using reference = T&;
+  using const_reference = const T&;
+  using size_type = size_t;
+
+  // Member functions
+  queue() { data_.clear(); }
+
+  queue(std::initializer_list<value_type> const &items) {
+    data_.clear();
+    for (const auto &i : items) {
+      push(i);
+    }
+  }
+
+  queue(const queue &q) { operator=(q); }
+
+  queue(queue &&q) { operator=(std::move(q)); }
+
+  ~queue() { data_.clear(); }
+
+  queue &operator=(queue &&q) {
+    this->data_ = std::move(q.data_);
+    return *this;
+  }
+
+  queue &operator=(const queue &q) {
+    this->data_ = q.data_;
+    return *this;
+  }
+
+  // Element access
+  const_reference front() { return data_.front(); }
+  const_reference back() { return data_.back(); }
+
+  // Capacity
+  bool empty() { return data_.empty(); }
+  size_type size() { return data_.size(); }
+
+  // Modifiers
+  void push(const_reference value) { data_.push_back(value); }
+  void pop() { data_.pop_front(); }
+  void swap(queue &other) { data_.swap(other.data_); }
+
+ private:
+  list<value_type> data_;
+};
+
+}  
 
 #endif // S21_QUEUE_H
