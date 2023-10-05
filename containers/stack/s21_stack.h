a11617cba195635cbd7589f9e0561666fe256ad5
Finch Marvolo <finchmar@va-c3.kzn.21-school.ru>
2023-10-05 19:58:13 +0300

    stack

--- containers/stack/s21_stack.h
+++ containers/stack/s21_stack.h
@@ -1,61 +1,66 @@
 #ifndef S21_STACK_H
 #define S21_STACK_H
 
-#include "../list/s21_list.h" 
+#include "../list/s21_list.h"
 
-namespace s21 {
+namespace s21
+{
 
-template <typename T>
-class stack {
- public:
+  template <typename T>
+  class stack
+  {
+  public:
+    // Stack Member type
+    using value_type = T;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = std::size_t;
 
- // Stack Member type
-  using value_type = T;
-  using reference = T&;
-  using const_reference = const T&;
-  using size_type = std::size_t;
+    // Stack Member functions
+    stack() : data_() {}
 
-  // Stack Member functions
-  stack() { data_.clear(); }
-
-  stack(std::initializer_list<value_type> const &items) {
-    data_.clear();
-    for (const auto &i : items) {
-      push(i);
+    stack(std::initializer_list<value_type> const &items)
+    {
+      data_.clear();
+      for (const auto &item : items)
+      {
+        push(item);
+      }
     }
-  }
 
-  stack(const stack &s) { operator=(s); }
+    stack(const stack &s) : data_(s.data_) {}
 
-  stack(stack &&s) { operator=(std::move(s)); }
+    stack(stack &&s) : data_(std::move(s.data_)) {}
 
-  ~stack() { data_.clear(); }
+    ~stack() {}
 
-  stack &operator=(stack &&s) {
-    data_ = std::move(s.data_);
-    return *this;
-  }
+    stack &operator=(const stack &s)
+    {
+      data_ = s.data_;
+      return *this;
+    }
 
-  stack &operator=(const stack &s) {
-    data_ = s.data_;
-    return *this;
-  }
+    stack &operator=(stack &&s)
+    {
+      data_ = std::move(s.data_);
+      return *this;
+    }
 
-  // Stack Element access
-  const_reference top() { return data_.front(); }
+    // Stack Element access
+    const_reference top() { return data_.front(); }
 
-  // Stack Capacity
-  bool empty() const { return data_.empty(); }
-  size_type size() const { return data_.size(); }
+    // Stack Capacity
+    bool empty() const { return data_.empty(); }
+    size_type size() const { return data_.size(); }
 
-  // Stack Modifiers
-  void push(const_reference value) { data_.push_front(value); }
-  void pop() { data_.pop_front(); }
-  void swap(stack &other) { data_.swap(other.data_); }
+    // Stack Modifiers
+    void push(const_reference value) { data_.push_front(value); }
+    void pop() { data_.pop_front(); }
+    void swap(stack &other) { data_.swap(other.data_); }
 
- private:
-  list<T> data_;
-};
-} 
+  private:
+    list<T> data_;
+  };
+}
 
 #endif // S21_STACK_H
